5dafb500a9099b388f417588d0197c48ccfb911d
RikuRuokonen <riku.ruokonen@helsinki.fi>
2021-06-15 13:26:45 +0300

    added Header with menu and placeholder for search button

---
+++ src/components/search/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import searchLogo from "../../images/search.svg";
+
+type SearchProps = {
+  searchTerm?: string;
+};
+
+const Search = ({ searchTerm }: SearchProps) => {
+  return (
+    <div className="w-20 flex border-l border-lightBlue self-stretch flex-row justify-start">
+      <img src={searchLogo} className="w-4 mx-auto" />
+    </div>
+  );
+};
+
+export default Search;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,54 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
+      colors: {
+        blue: {
+          DEFAULT: "#253764",
+        },
+        lightBlue: {
+          DEFAULT: "#93D4F0",
+        },
+        gray: {
+          DEFAULT: "#EAEAEA",
+          light: "#F7F7F7",
+        },
+        ageYellow: {
+          DEFAULT: "#F5EA2E",
+        },
+        ageOrange: {
+          DEFAULT: "#D4791EE",
+        },
+        agePurple: {
+          DEFAULT: "#5E0F75",
+        },
+        ageGreen: {
+          DEFAULT: "#33652E",
+        },
+        ageBrown: {
+          DEFAULT: "#6E470A",
+        },
+        ageRed: {
+          DEFAULT: "#B9382C",
+        },
+      },
+      spacing: {
+        1: "8px",
+        2: "12px",
+        3: "16px",
+        4: "24px",
+        5: "32px",
+        6: "48px",
+      },
+    },
+    fontFamily: {
+      tondu: ["Tondu"],
+      sourceSansPro: ["SourcePro"],
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
