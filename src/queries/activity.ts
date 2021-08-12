d182025ab3a27b11f2a574299be68a235f3ad206
RikuRuokonen <riku.ruokonen@helsinki.fi>
2021-08-12 12:22:03 +0300

    added queries for activity suggestions

--- src/queries/activity.ts
+++ src/queries/activity.ts
@@ -1,42 +1,52 @@
 import { graphql, useStaticQuery } from 'gatsby';
 
 export const getAllActivities = `
-  {
-    allStrapiActivity {
-      edges {
-        node {
-          id
-          educational_objectives {
-            name
-            slug
-          }
-          leader_skills {
-            name
-            slug
-          }
+{
+  allStrapiActivity {
+    edges {
+      node {
+        id
+        educational_objectives {
+          name
+          slug
+        }
+        leader_skills {
+          name
+          slug
+        }
+        title
+        content
+        mandatory
+        activity_group {
+          age_group
           title
+        }
+        ingress
+        duration {
+          name
+          slug
+        }
+        skill_areas {
+          name
+          slug
+        }
+        locations {
+          name
+          slug
+        }
+        leader_tasks
+        suggestions {
+          author
+          id
+          like_count
+          title
+          activity
           content
-          mandatory
-          activity_group {
-            age_group
-            title
-          }
-          ingress
-          duration {
-            name
-            slug
-          }
-          skill_areas {
-            name
-            slug
-          }
-          locations {
-            name
-            slug
-          }
-          leader_tasks
+          published_at(locale: "FI")
         }
       }
     }
   }
+}
+
 `;
