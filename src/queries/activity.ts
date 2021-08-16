5fee489f2c9fa61a1d97c4279b33198348232778
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-08-16 15:57:00 +0300

    Fix activity query

--- src/queries/activity.ts
+++ src/queries/activity.ts
@@ -1,48 +1,46 @@
 export const getAllActivities = `
-  {
-    allStrapiActivity {
-      nodes {
-        id
-        educational_objectives {
-          name
-          slug
-        }
-        leader_skills {
-          name
-          slug
-        }
+{
+  allStrapiActivity {
+    nodes {
+      id
+      educational_objectives {
+        name
+        slug
+      }
+      leader_skills {
+        name
+        slug
+      }
+      title
+      content
+      mandatory
+      activity_group {
+        age_group
         title
+      }
+      ingress
+      duration {
+        name
+        slug
+      }
+      skill_areas {
+        name
+        slug
+      }
+      locations {
+        name
+        slug
+      }
+      leader_tasks
+      suggestions {
+        author
+        id
+        like_count
+        title
+        activity
         content
-        mandatory
-        activity_group {
-          age_group
-          title
-        }
-        ingress
-        duration {
-          name
-          slug
-        }
-        skill_areas {
-          name
-          slug
-        }
-        locations {
-          name
-          slug
-        }
-        leader_tasks
-        suggestions {
-          author
-          id
-          like_count
-          title
-          activity
-          content
-        }
       }
     }
   }
 }
-
 `;
