2f842067ebea24257330cc272e1fca0ac200657d
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-10-04 17:31:20 +0300

    Fix i18n ssr maybe

--- configs/i18n.ts
+++ configs/i18n.ts
@@ -3,7 +3,7 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from "react-i18next";
 
-i18n
+export const setupI18n = async (ssr?: boolean) => i18n
   .use(initReactI18next)
   .use(Backend)
   .init({
@@ -12,12 +12,12 @@ i18n
     interpolation: {
       escapeValue: false,
     },
-    backend: {
+    backend: !ssr ? {
       loadPath: `${process.env.GATSBY_API_URL}/settings/translations/{{lng}}`
-    },
+    } : undefined,
     react: {
       useSuspense: false,
     }
-  })
+  });
 
 export default i18n;
