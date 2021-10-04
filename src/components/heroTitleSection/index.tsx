eba8f35c29145c32c6d3ae6aed36c11de80a8bfc
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-10-04 11:57:15 +0300

    Hide hero logo if no image is given

--- src/components/heroTitleSection/index.tsx
+++ src/components/heroTitleSection/index.tsx
@@ -1,12 +1,11 @@
 import clsx from 'clsx';
 import React from 'react';
-import mockHero from '../../images/mockHero.png';
 import { hexToRgba } from '../../utils/color';
 import Container from '../container';
 
 type HeroTitleSectionProps = {
   mainImageUrl?: string;
-  logoUrl: string;
+  logoUrl?: string;
   smallMainTitle?: boolean;
   mainTitle?: string;
   subTitle?: string;
@@ -34,20 +33,26 @@ const HeroTitleSection = ({
       })}
     >
       <Container className="flex">
-        <div
-          className="flex w-44 h-44 rounded-4xl align-top p-3"
-          style={{ backgroundColor: hexToRgba(color || '', 0.6) }}
-        >
-          <div className="bg-white rounded-3xl flex flex-col align-center justify-center items-center w-full">
-            <img className="w-24" src={logoUrl}></img>
+        {logoUrl && (
+          <div
+            className="flex w-44 h-44 rounded-4xl align-top p-3 mr-4"
+            style={{ backgroundColor: hexToRgba(color || '', 0.6) }}
+          >
+            <div className="bg-white rounded-3xl flex flex-col align-center justify-center items-center w-full">
+              <img className="w-24" src={logoUrl} alt={mainTitle} title={mainTitle}></img>
+            </div>
           </div>
-        </div>
-        <div className="ml-4 align-top flex flex-col justify-center">
+        )}
+        <div className="align-top flex flex-col justify-center">
           <div className="h-16 flex items-center">
-            <h1 className={clsx('tracking-wider leading-8 break-words uppercase', {
-              'text-2rem': smallMainTitle,
-              'text-white': mainImageUrl,
-            })}>{mainTitle}</h1>
+            <h1
+              className={clsx('tracking-wider leading-8 break-words uppercase', {
+                'text-2rem': smallMainTitle,
+                'text-white': mainImageUrl,
+              })}
+            >
+              {mainTitle}
+            </h1>
           </div>
           <div className="h-16 flex items-center">
             <span className="text-blue text-2xl inline-block">{subTitle}</span>
