b577262648a9a2309f555698178d3563970c3d08
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-10-05 17:01:09 +0300

    Fix general search result typename

--- src/components/basicCard/index.tsx
+++ src/components/basicCard/index.tsx
@@ -3,6 +3,7 @@ import Card from '../card';
 import { prependApiUrl, removeHtml } from '../../utils/helpers';
 import ClampLines from 'react-clamp-lines';
 import { v4 as uuidv4 } from 'uuid';
+import Pill from '../pill';
 
 interface BasicCardProps {
   image?: string;
@@ -14,10 +15,14 @@ interface BasicCardProps {
 
 function BasicCard({ image, title, text, link, typeName }: BasicCardProps) {
   return (
-    <Card link={link} padded={false} backGround={false} noMinHeight rounded={false} >
+    <Card link={link} padded={false} backGround={false} noMinHeight rounded={false}>
       {image && <img src={prependApiUrl(image)} className="rounded-2xl h-48 object-cover mb-2"></img>}
-      {typeName && <span className="text-sm">{typeName}</span>}
-      {title && <h4 className="mb-3 uppercase break-words">{title}</h4>}
+      {typeName && (
+        <div className="flex">
+          <Pill className="text-sm py-0.5 px-1">{typeName}</Pill>
+        </div>
+      )}
+      {title && <h4 className="mb-3 mt-1 uppercase break-words">{title}</h4>}
       {text && <ClampLines text={removeHtml(text)} id={`basic-card-${uuidv4()}-text`} lines={7} buttons={false} />}
     </Card>
   );
