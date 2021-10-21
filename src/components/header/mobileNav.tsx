f72a3be9f36c53798460c4c45113787dff887672
Linnea Myllynen <linnea.myllynen@orangit.fi>
2021-10-21 09:43:27 +0300

    Add link to front page to partio logo in mobile view

--- src/components/header/mobileNav.tsx
+++ src/components/header/mobileNav.tsx
@@ -33,7 +33,7 @@ const MobileNav = ({
           className="w-6 inline-block border-lightBlue border-r p-2"
           onClick={toggleHamburger}
         />
-        {!state.searchActive && <img src={logo} className="mx-4 w-24" />}
+        {!state.searchActive && <a href='/' className="flex flex-row"><img src={logo} className="mx-4 w-24"/></a>}
         <div className="flex flex-row space-x-4 cursor-default ml-auto">
           <LocaleSelector
             currentDropDownOpen={currentDropDownOpen}
