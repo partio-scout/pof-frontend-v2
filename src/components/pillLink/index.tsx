134d448d33f6892a353844f0721ab0d4b3821c8a
Timo Aho <timo.aho@orangit.fi>
2021-11-15 15:12:45 +0200

    Add attachmentIcon to activityGroup template

--- src/components/pillLink/index.tsx
+++ src/components/pillLink/index.tsx
@@ -7,23 +7,25 @@ interface PillLinkProps {
   to: string;
   color?: string | null;
   children: React.ReactNode;
+  attachmentIcon?: boolean;
 }
 
-function PillLink({ to, children, color }: PillLinkProps) {
+function PillLink({ to, children, color, attachmentIcon }: PillLinkProps) {
   // TODO: display block vs flex
   return (
     <CombinedLink
       to={to}
-      className="block flex flex-row bg-gray-light rounded-2xl p-3 md:p-3 font-tondu text-2xl uppercase tracking-wider align-middle"
+      className="block flex flex-row bg-gray-light rounded-2xl p-3 md:p-3 font-tondu text-2xl uppercase tracking-wider align-center justify-center"
     > 
-    <div 
-      className="flex w-12 h-12 rounded-md p-3 mr-4"
-      style={{ backgroundColor: hexToRgba(color || '', 0.6) }}
-    >
-      <AttachmentIcon className="h-70 w-50"/>
-    </div>
+    {attachmentIcon &&
+      <div className="flex w-12 h-12 rounded-md p-2 mr-4" style={{ backgroundColor: hexToRgba(color || '', 0.6) }}>
+        <AttachmentIcon className="h-4 w-4 self-center"/>
+      </div>
+    }
+    <div className="self-center">
       {children}
-    </CombinedLink> 
+    </div>
+  </CombinedLink> 
   );
 }
 
