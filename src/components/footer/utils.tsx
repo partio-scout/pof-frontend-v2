ef2a638d01e478bdbb3a39a1c75a1ec37ad0cd39
Linnea Myllynen <linnea.myllynen@orangit.fi>
2021-11-22 10:26:23 +0200

    Add footer styled

--- src/components/footer/utils.tsx
+++ src/components/footer/utils.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 /**
  * Replaces newlines with html line-breaks (<br>)
  */
-export const addLineBreaks = (text?: string | null) =>
+export const addLineBreaks = (text?: string | null, style?: string | undefined) =>
   text?.split('\n').map((span, i, arr) => (
-    <span key={span}>
+    <span className={style} key={span}>
       {span}
       {i !== arr.length - 1 && <br />}
     </span>
