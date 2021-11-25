93dc04e5424a11c9e0f5491e4fc696d5f8f8d044
Linnea Myllynen <linnea.myllynen@orangit.fi>
2021-11-25 14:10:31 +0200

    Add Helmet tags for cookiepage

--- src/pages/cookiepage.tsx
+++ src/pages/cookiepage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { graphql, PageProps } from 'gatsby';
 import Layout from '../layouts/default';
+import Helmet from 'react-helmet';
 import { StrapiCookiePage } from '../../graphql-types';
 import { currentLocale } from '../utils/helpers';
 
@@ -11,12 +12,14 @@ const CookiePageComponent = ({ page }: { page?: StrapiCookiePage }) => {
     <div className="flex flex-col items-center my-10">
       <h1 className="uppercase sm:text-xxxlt md:text-xxxlw text-center">{title}</h1>
       {ingress && <p className="my-4 md:text-base">{ingress}</p>}
-      <script
-        id="CookieDeclaration"
-        src={`https://consent.cookiebot.com/${process.env.GATSBY_COOKIEBOT_ID}/cd.js`}
-        type="text/javascript"
-        defer
-      ></script>
+      <Helmet>
+        <script
+          id="CookieDeclaration"
+          src={`https://consent.cookiebot.com/${process.env.GATSBY_COOKIEBOT_ID}/cd.js`}
+          type="text/javascript"
+          defer
+        ></script>
+      </Helmet>
     </div>
   )
 }
