db04d369402f0a61879f227254a1a60642000af5
Linnea Myllynen <linnea.myllynen@orangit.fi>
2021-11-26 12:36:30 +0200

    Add async tag

--- src/pages/cookiepage.tsx
+++ src/pages/cookiepage.tsx
@@ -12,12 +12,12 @@ const CookiePageComponent = ({ page }: { page?: StrapiCookiePage }) => {
     <div className="flex flex-col items-center my-10">
       <h1 className="uppercase sm:text-xxxlt md:text-xxxlw text-center">{title}</h1>
       {ingress && <p className="my-4 md:text-base">{ingress}</p>}
-        <script
-          id="CookieDeclaration"
-          src={`https://consent.cookiebot.com/${process.env.GATSBY_COOKIEBOT_ID}/cd.js`}
-          type="text/javascript"
-          defer
-        ></script>
+      <script
+        id="CookieDeclaration"
+        src={`https://consent.cookiebot.com/${process.env.GATSBY_COOKIEBOT_ID}/cd.js`}
+        type="text/javascript"
+        async
+      ></script>
     </div>
   )
 }
