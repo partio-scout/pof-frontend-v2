8aa3cfec3cf5dd1c7025474b37ad2f50de84f219
Linnea Myllynen <linnea.myllynen@orangit.fi>
2021-11-30 09:52:26 +0200

    Update cookiepage.tsx

--- src/pages/cookiepage.tsx
+++ src/pages/cookiepage.tsx
@@ -1,37 +1,31 @@
 import * as React from 'react';
 import { graphql, PageProps } from 'gatsby';
 import Layout from '../layouts/default';
-import Helmet from 'react-helmet';
 import { StrapiCookiePage } from '../../graphql-types';
 import { currentLocale } from '../utils/helpers';
 
-const CookiePageComponent = ({ page }: { page?: StrapiCookiePage }) => {
-  const { title, ingress } = page || {};
-
-  return (
-    <div className="flex flex-col items-center my-10">
-      <h1 className="uppercase sm:text-xxxlt md:text-xxxlw text-center">{title}</h1>
-      {ingress && <p className="my-4 md:text-base">{ingress}</p>}
-      <script
-        id="CookieDeclaration"
-        src={`https://consent.cookiebot.com/${process.env.GATSBY_COOKIEBOT_ID}/cd.js`}
-        type="text/javascript"
-      ></script>
-    </div>
-  )
-}
 
 const CookiePage = ({ data }: PageProps<{ pages: { nodes: StrapiCookiePage[] } }>) => {
   const locale = currentLocale();
-
   const pageData = data.pages.nodes.find((page) => page.locale === locale);
+  const { title, ingress } = pageData || {};
+
 
   return (
     <Layout>
-      <CookiePageComponent page={pageData} />
+      <div className="flex flex-col items-center my-10">
+        <h1 className="uppercase sm:text-xxxlt md:text-xxxlw text-center">{title}</h1>
+        {ingress && <p className="my-4 md:text-base">{ingress}</p>}
+        <script
+          id="CookieDeclaration"
+          src={`https://consent.cookiebot.com/${process.env.GATSBY_COOKIEBOT_ID}/cd.js`}
+          type="text/javascript"
+        ></script>
+      </div>
     </Layout>
   );
 }; 
+
 export default CookiePage
 
 export const query = graphql`
@@ -46,3 +40,4 @@ export const query = graphql`
     }
   }
 `;
+
