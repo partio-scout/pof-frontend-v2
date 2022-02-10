ae74b1dabde1aacc076661c421d3630238cdf82d
Mika Kattainen <mika.kattainen@orangit.fi>
2022-02-10 09:08:39 +0200

    Revert "Fix linter issues in algolia.ts"
    This reverts commit 9e82c236df27c416e8c0e448c3d36c1f6286fe8c.

--- src/utils/algolia.ts
+++ src/utils/algolia.ts
@@ -1,4 +1,4 @@
-import algoliasearch from 'algoliasearch/lite';
+import algoliasearch from "algoliasearch/lite";
 
 const appId = process.env.GATSBY_ALGOLIA_APP_ID || '';
 const apiKey = process.env.GATSBY_ALGOLIA_API_KEY || '';
