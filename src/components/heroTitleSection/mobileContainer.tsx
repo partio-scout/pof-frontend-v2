60b7c12909fff909ea45fa67c13b4dd8b0031f4b
Linnea Myllynen <linnea.myllynen@orangit.fi>
2022-03-08 13:57:56 +0200

    Update styling of mobile herotitleContainer

--- src/components/heroTitleSection/mobileContainer.tsx
+++ src/components/heroTitleSection/mobileContainer.tsx
@@ -23,35 +23,33 @@ const MobileHeroTitleContainer = ({
   return (
     <div className="relative block sm:hidden">
       {mainImageUrl && (
-        <div className="flex relative h-80 mb-5">
+        <div className="flex relative h-80 mb-5 justify-center items-center">
           <div className="bg-gradient-to-t from-blue w-full h-full absolute opacity-75"></div>
-          <div style={{ background: `url(${mainImageUrl}) center/cover no-repeat` }} className="w-full h-full">
-            {logoUrl && (
-              <div
-                className="flex z-10 w-44 h-44 rounded-4xl p-1 m-auto"
-                style={{ backgroundColor: hexToRgba(color || '', 0.6) }}
-              >
-                <div className="bg-white rounded-3xl flex flex-col align-center justify-center items-center w-full f-full">
-                  <img className="w-24" src={logoUrl} alt={mainTitle} title={mainTitle}></img>
-                </div>
+          <div style={{ background: `url(${mainImageUrl}) center/cover no-repeat` }} className="w-full h-full"></div>
+          {logoUrl && (
+            <div
+              className="flex absolute w-44 h-44 rounded-4xl p-1 m-auto"
+              style={{ backgroundColor: hexToRgba(color || '', 0.6) }}
+            >
+              <div className="bg-white rounded-3xl flex flex-col align-center justify-center items-center w-full f-full">
+                <img className="w-24" src={logoUrl} alt={mainTitle} title={mainTitle}></img>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       )}
-        <Container className="flex flex-col sm:flex-row items-center">
+        <Container className="flex flex-col px-10 items-start">
           <div className="align-top flex flex-col justify-center">
-            <div className="h-16 flex items-end">
-              <h1
-                className={clsx('tracking-wider leading-8 break-words uppercase sm:text-xxxlt md:text-xxxlw', {
+            <div className="min-h-12 min-w-0 flex items-center">
+              <h2
+                className={clsx('tracking-wider leading-8 break-words uppercase', {
                   'text-2rem': smallMainTitle,
-                  'sm:text-white': mainImageUrl,
                 })}
               >
-                {mainTitle}jeeee
-              </h1>
+                {mainTitle}
+              </h2>
             </div>
-            <div className="h-16 flex items-center">
+            <div className="min-h-12 flex items-center">
               <span className="text-blue text-2xl inline-block">{subTitle}</span>
             </div>
           </div>
