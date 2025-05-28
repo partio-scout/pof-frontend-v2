1a10d146a781311fb77c5d7e1107838001da8c4d
Jaakko Mäkelä <jaakko.makela@orangit.fi>
2025-05-28 15:00:57 +0300

    Set queryLimit to 100

--- configs/gatsby-config.ts
+++ configs/gatsby-config.ts
@@ -21,7 +21,7 @@ const config: GatsbyConfig = {
         collectionTypes: [
           {
             singularName: 'activity',
-            queryLimit: 25,
+            queryLimit: 100,
             queryParams: {
               populate: {
                 activity_group: '*',
@@ -53,7 +53,7 @@ const config: GatsbyConfig = {
           },
           {
             singularName: 'age-group',
-            queryLimit: 25,
+            queryLimit: 100,
             queryParams: {
               populate: {
                 activity_groups:  {
@@ -222,7 +222,7 @@ const config: GatsbyConfig = {
           },           
           {
             singularName: 'duration',
-            queryLimit: 25,
+            queryLimit: 100,
             queryParams: {
               populate: '*'
             },
@@ -235,7 +235,7 @@ const config: GatsbyConfig = {
           
           {
             singularName: 'location',
-            queryLimit: 25,
+            queryLimit: 100,
             queryParams: {
               populate: '*'
             },
@@ -248,7 +248,7 @@ const config: GatsbyConfig = {
           
           {
             singularName: 'activity-group',
-            queryLimit: 25,
+            queryLimit: 100,
             queryParams: {
               populate: {
                 activities: {
@@ -286,7 +286,7 @@ const config: GatsbyConfig = {
 
           {
             singularName: 'content-page',
-            queryLimit: 25,
+            queryLimit: 100,
             queryParams: {
               populate: {
                 title: '*',
@@ -380,7 +380,7 @@ const config: GatsbyConfig = {
           },
           {
             singularName: 'suggestion',
-            queryLimit: 25,
+            queryLimit: 100,
             queryParams: {
               populate: '*',
               locations: {
